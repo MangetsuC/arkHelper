a494a29bdf708d59e0872deef201cac519aec9ec
MangetsuC <demando-ok@outlook.com>
2020-05-04 17:52:54 +0800

    fit new gui

--- foo/arknight/Battle.py
+++ foo/arknight/Battle.py
@@ -7,10 +7,10 @@ from foo.pictureR import pictureFind
 from foo.win import toast
 
 class BattleLoop:
-    def __init__(self, adb, cwd, app):
+    def __init__(self, adb, cwd, ico):
         self.cwd = cwd
         self.adb = adb
-        self.app = app
+        self.ico = ico
         self.listBattleImg = listdir(cwd + "/res/battle")
         self.switch = False
         self.connectSwitch = False
@@ -28,37 +28,20 @@ class BattleLoop:
                 return False
         
         else:
+            toast.broadcastMsg("ArkHelper", "连接模拟器失败，请检查设置和模拟器", self.ico)
             return False
 
 
     def run(self, switchI):
         isFirstTurn = True
         self.switch = switchI
-        '''self.connectSwitch = True
-        for times in range(20):
-            if self.connectSwitch:
-                self.switch = self.adb.connect()
-                if self.switch:
-                    break
-                times += 1
-            else:
-                break'''
-        
-        #print(self.switch)
-        if (not self.switch) and self.connectSwitch:
-            #self.app.setButton(1)
-            self.app.setState("连接失败，请检查配置文件或重启模拟器")
-            toast.broadcastMsg("ArkHelper", "连接失败，请检查配置文件或重启模拟器", self.app.icon)
-        else:
+
+        if self.switch:
             twiceTry = 0
-            while self.switch and self.app.mainGate:
-                self.app.setState("正在获取并分析屏幕信息")
-                
+            while self.switch:
                 isSSSuccess = self.adb.screenShot(pngName = 'check')
                 if not isSSSuccess:
-                    self.app.setState("获取屏幕信息失败，请重启模拟器")
-                    toast.broadcastMsg("ArkHelper", "获取屏幕信息失败，请重启模拟器", self.app.icon)
-                    #self.app.setButton(1)
+                    toast.broadcastMsg("ArkHelper", "获取屏幕信息失败，请重启模拟器", self.ico)
                     self.switch = False
                     break
 
@@ -87,16 +70,12 @@ class BattleLoop:
 
                         isSSSuccess = self.adb.screenShot()
                         if not isSSSuccess:
-                            self.app.setState("获取屏幕信息失败，请重启模拟器")
-                            toast.broadcastMsg("ArkHelper", "获取屏幕信息失败，请重启模拟器", self.app.icon)
-                            #self.app.setButton(1)
+                            toast.broadcastMsg("ArkHelper", "获取屏幕信息失败，请重启模拟器", self.ico)
                             self.switch = False
                             break
                         picAutoOn = pictureFind.matchImg(self.screenShot, self.autoOn)
                         if picAutoOn == None:
-                            self.app.setState("无法勾选代理指挥")
-                            toast.broadcastMsg("ArkHelper", "无法勾选代理指挥", self.app.icon)
-                            #self.app.setButton(1)
+                            toast.broadcastMsg("ArkHelper", "无法勾选代理指挥", self.ico)
                             self.switch = False
                             break
 
@@ -115,9 +94,7 @@ class BattleLoop:
                             self.adb.click(picPos[0], picPos[1], isSleep = False)
                             if eachObj == "cancel.png":
                                 self.switch = False
-                                self.app.setState("理智耗尽")
-                                toast.broadcastMsg("ArkHelper", "理智耗尽", self.app.icon)
-                                #self.app.setButton(1)
+                                toast.broadcastMsg("ArkHelper", "理智耗尽", self.ico)
                             break
                 #sleep(1)
     def stop(self):
