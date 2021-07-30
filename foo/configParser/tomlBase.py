c5b3cfcd8293429f3d4082fbc09254cbce45ebf4
MangetsuC <demando-ok@outlook.com>
2021-07-30 19:28:36 +0800

    toml的读写

--- foo/configParser/tomlBase.py
+++ foo/configParser/tomlBase.py
@@ -37,30 +37,60 @@ def defaultConfig():
     meetingroom.daily = true #会客室是否自动领取每日线索（会客室右侧3选项中的第一个）
 
     [logistic.threshold]
-    work = 0
-    dorm = 24
+    work = 0 #基建中心情低于此值的工作中干员会被撤下
+    dorm = 24 #宿舍中心情高于此值的干员会被撤下
 
     [function]
-    battle.default = true
-    schedule.default = false
+    loop.default = true #循环作战是否默认打开，下同
+    schedule.default = false #计划作战
 
-    autopc.default = false
-    autopc.skip1star = true
-    autopc.skip5star = true
-    autopc.skip23star = false
+    autopc.default = false #自动公招
+    autopc.skip1star = true #自动公招保留1星tag组合
+    autopc.skip5star = true #自动公招保留5星tag组合
+    autopc.skip23star = false #自动公招保留2，3星tag组合
+    #自动公招始终会保留6星tag高级资深干员
     
-    task.default = true
-    credit.default = false
-    shutdown.default = false
+    task.default = true #自动交付任务
+    credit.default = false #自动访问好友获取信用点
+    shutdown.default = false #自动关机
 
     [recover]
-    loop.medicine.enable = false
-    loop.stone.enable = false
-    loop.stone.max = 0
+    loop.medicine.enable = false #循环作战中使用理智药剂恢复理智
+    loop.stone.enable = false #循环作战中使用源石恢复理智
+    loop.stone.max = 0 #循环作战中使用源石的最大数量
 
-    schedule.medicine.enable = false
-    schedule.stone.enable = false
-    schedule.stone.max = 0
+    schedule.medicine.enable = false #计划作战中使用理智药剂恢复理智
+    schedule.stone.enable = false #计划作战中使用源石恢复理智
+    schedule.stone.max = 0 #计划作战中使用源石的最大数量
+    '''
+    return loads(default)
+
+def defaultSimulator():
+    default = '''
+    [bluestacks]
+    ip = '127.0.0.1:5555'
+    adb = 'internal'
+    name = '蓝叠(设置中开启adb)'
+
+    [mumu]
+    ip = '127.0.0.1:7555'
+    adb = 'internal'
+    name = '蓝色mumu(旧版)'
+
+    [yeshen]
+    ip = '*req*'
+    adb = 'external'
+    name = '夜神模拟器'
+
+    [leidian]
+    ip = 'emulator-5554'
+    adb = 'internal'
+    name = '雷电模拟器'
+
+    [xiaoyao]
+    ip = '127.0.0.1:21503'
+    adb = 'internal'
+    name = '逍遥模拟器'
     '''
     return loads(default)
 
@@ -83,7 +113,7 @@ def ini2toml():
         config_new['logistic']['threshold'] = {'work': config_old['logistic']['moodthreshold'],
                                             'dorm': config_old['logistic']['dormthreshold']}
         config_new['function'] = dict()
-        config_new['function']['battle'] = {'default': config_old['function']['battle']}
+        config_new['function']['loop'] = {'default': config_old['function']['battle']}
         config_new['function']['schedule'] = {'default': config_old['function']['schedule']}
         config_new['function']['autopc'] = {'default': config_old['function']['autopc'],
                                             'skip1star': config_old['function']['autopc_skip1star'],
@@ -109,10 +139,11 @@ def ini2toml():
     
     return config_new
 
-def tomlRead():
+def tomlRead(filename):
+    '在两个不同的目录下读取相关文件'
     toml = dict()
-    releasePath = f'C:/Users/{getlogin()}/AppData/Roaming/arkhelper/config.toml'
-    devPath = './config.toml'
+    releasePath = f'C:/Users/{getlogin()}/AppData/Roaming/arkhelper/{filename}' #小助手各类配置通常的存储位置
+    devPath = f'./{filename}' #当前目录下的配置文件会优先读取，这样可以不干扰日常使用
     if path.exists(devPath):
         with open(devPath, 'r', encoding='UTF-8') as f:
             toml = loads(f.read())
--- foo/configParser/tomlParser.py
+++ foo/configParser/tomlParser.py
@@ -9,11 +9,10 @@ from foo.configParser import tomlBase
 
 
 class Toml:
-    releasePath = f'C:/Users/{getlogin()}/AppData/Roaming/arkhelper/config.toml'
-    devPath = './config.toml'
-    def __init__(self):
-        self.toml = tomlBase.tomlRead()
-        default_toml = tomlBase.defaultConfig()
+    def __init__(self, filename, default_toml):
+        self.releasePath = f'C:/Users/{getlogin()}/AppData/Roaming/arkhelper/' + filename
+        self.devPath = './' + filename
+        self.toml = tomlBase.tomlRead(filename)
         if self.toml == dict():
             ini = tomlBase.ini2toml()
             self.toml = tomlBase.dictUpdate(default_toml, ini)
@@ -30,6 +29,48 @@ class Toml:
             with open(self.releasePath, 'w', encoding='UTF-8') as f:
                 f.write(dumps(self.toml))
 
+    def get(self, key):
+        '获取值'
+        keys = key.split('.')
+        temp = self.toml
+        for i in keys:
+            temp = temp[i]
+        return temp
+
+    def change(self, key, value):
+        '修改或增加条目'
+        keys = key.split('.')
+        temp = self.toml
+        for i in keys[:-1]:
+            if not i in temp.keys():
+                temp[i] = dict()
+            temp = temp[i]
+        temp[keys[-1]] = value
+        self.write()
+
+    def delete(self, key):
+        '删除某一条目'
+        keys = key.split('.')
+        temp = self.toml
+        for i in keys[:-1]:
+            if not i in temp.keys():
+                break #找不到对应键直接返回
+            temp = temp[i]
+        else:
+            temp.pop(keys[-1])
+        self.write()
+
+
+
+class configToml(Toml):
+    def __init__(self):
+        super(configToml, self).__init__('config.toml', tomlBase.defaultConfig())
+
+class simulatorToml(Toml):
+    def __init__(self):
+        super(simulatorToml, self).__init__('simulator.toml', tomlBase.defaultSimulator())
+
 if __name__ == '__main__':
-    test = Toml()
+    test1 = configToml()
+    test2 = simulatorToml()
 
